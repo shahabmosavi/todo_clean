f7e2f7d2954e803fc2f79ebade9c707ecad3e381
Your Name <you@example.com>
2022-04-28 15:57:26 +0430

    added todo placeholder

--- lib/Screens/Todo/Presentation/Pages/todo_page.dart
+++ lib/Screens/Todo/Presentation/Pages/todo_page.dart
@@ -7,6 +7,32 @@ class TodoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Todo')),
+      body: const Body(),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+      child: Column(
+        children: [
+          Expanded(flex: 10, child: Placeholder()),
+          const SizedBox(height: 10),
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(flex: 4, child: Placeholder()),
+              const SizedBox(width: 10),
+              Expanded(child: Placeholder()),
+            ],
+          )),
+        ],
+      ),
     );
   }
 }
